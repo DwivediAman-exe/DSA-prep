5b9c39f84808470aa42b47a316a4d4d3c89148d1
prakharritik <60786008+prakharritik@users.noreply.github.com>
2022-01-23 15:21:10 +0530

    Added Max sum in the configuration

--- Hashing/Largest subarray with 0 sum.cpp
+++ Hashing/Largest subarray with 0 sum.cpp
@@ -1,4 +1,4 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 /*
 Question Link - https://practice.geeksforgeeks.org/problems/largest-subarray-with-0-sum/1/
@@ -15,43 +15,47 @@ Method 1:
 
 TC - O(N*N) SC-(1)
 */
-class Solution{
-    public:
-    int maxLen(vector<int>&A, int n)
-    {   
-        int len=0;
-        
-        for(int i=0;i<n;i++){
-            int s=0;
-            for(int j=i;j<n;j++){
-                s+=A[j];
-                if(s==0)len=max(len,j-i+1);
+class Solution
+{
+public:
+    int maxLen(vector<int> &A, int n)
+    {
+        int len = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            int s = 0;
+            for (int j = i; j < n; j++)
+            {
+                s += A[j];
+                if (s == 0)
+                    len = max(len, j - i + 1);
             }
         }
         return len;
     }
 };
 
-
 /*
 Method 2:
 
 TC - O(N) SC-O(N)
 */
-class Solution{
-    public:
-    int maxLen(vector<int>&A, int n)
-    {   
-        int s=0,len=0;
-        unordered_map<int,int> mp;
-        mp[0]=-1;
-        for(int i=0;i<n;i++)
+class Solution
+{
+public:
+    int maxLen(vector<int> &A, int n)
+    {
+        int s = 0, len = 0;
+        unordered_map<int, int> mp;
+        mp[0] = -1;
+        for (int i = 0; i < n; i++)
         {
-            s+=A[i];
-            if(mp.find(s)==mp.end())
-                mp[s]=i;
-            else 
-                len=max(len,i-mp[s]);
+            s += A[i];
+            if (mp.find(s) == mp.end())
+                mp[s] = i;
+            else
+                len = max(len, i - mp[s]);
         }
         return len;
     }
